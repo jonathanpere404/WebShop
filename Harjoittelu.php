9d4ec360c82d9ac5a377a4636d320e78b9a91a6f
Jonathan Pere <jp.jonttu.jp@gmail.com>
2018-11-21 23:11:48 +0200

    commit

---
+++ Harjoittelu.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Moikka
+</title>
+</head>
+<body> 
+<?php
+$myArr = array("John", "Mary", "Peter", "Sally");
+
+$myJSON = json_encode($myArr);
+
+echo $myJSON;
+?>
+</body>
+</html>
